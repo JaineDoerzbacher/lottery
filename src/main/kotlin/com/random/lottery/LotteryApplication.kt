913cafcec3fcf630fb892d21f09849049eaf777c
JaineDoerzbacher <doerzbacherjaine@gmail.com>
2021-01-18 20:31:49 -0300

    Primeiro commit

---
+++ src/main/kotlin/com/random/lottery/LotteryApplication.kt
@@ -0,0 +1,11 @@
+package com.random.lottery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LotteryApplication
+
+fun main(args: Array<String>) {
+	runApplication<LotteryApplication>(*args)
+}
