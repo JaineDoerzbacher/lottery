c5dfdd692cd0105e325f4dcb198371f96f3acafb
JaineDoerzbacher <doerzbacherjaine@gmail.com>
2021-01-18 20:39:39 -0300

    Criação da entidade Aposta

---
+++ src/main/kotlin/com/random/lottery/model/Aposta.kt
@@ -0,0 +1,14 @@
+package com.random.lottery.model
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "APOSTAS")
+data class Aposta(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+    val email: String,
+    val numeroAposta: Long
+)
